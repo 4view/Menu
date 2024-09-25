b2169c2576d4ffe13dde2012a10be387edf78e30
4view <akochergin.2002@gmail.com>
2024-09-25 14:38:54 +0500

    feat: Add dish class

---
+++ Model/Dish.cs
@@ -0,0 +1,12 @@
+class Dish
+{
+    public Dish (string name, DateTime lastPreparation)
+    {
+        Name = name;
+        LastPreparation = lastPreparation;
+    }
+
+    public string Name { get; }
+
+    public DateTime LastPreparation { get; }
+}
