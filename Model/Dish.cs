efd4c35d419ac3b58b2db6311fbaa25237bd8172
4view <akochergin.2002@gmail.com>
2024-09-26 12:35:46 +0500

    feat: Add 'type' property

--- Model/Dish.cs
+++ Model/Dish.cs
@@ -1,12 +1,17 @@
+using System.Reflection.Metadata;
+
 namespace Menu.Model;
 
 class Dish
 {
-    public Dish (string name, DateTime lastPreparation)
+    public Dish (int type, string name, DateTime lastPreparation)
     {
-        Name = name;
+        Type = type;
+        Name = name.Trim();
         LastPreparation = lastPreparation;
     }
+
+    public int Type { get; }
 
     public string Name { get; }
 
