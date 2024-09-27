d7bcdbed6e3cf24d91fff0b284f465deaf67e493
4view <akochergin.2002@gmail.com>
2024-09-27 13:30:22 +0500

    feat: Аdd validation

--- Program.cs
+++ Program.cs
@@ -17,34 +17,58 @@ class Program
         FileReader fileReade = new FileReader();
         List<string> lines = fileReade.ReadFile(path);
 
-        Console.Write("Введите число от скольки дней блюдо подходит к фильтрации: ");
-        var userDays = int.Parse(Console.ReadLine());
+        bool foundDishes = false;
 
-        Console.Write("Введите тип блюда (1/2): ");
-        var userType = (SortBy)int.Parse(Console.ReadLine());
-
-        //Создаем экземпляр класса DishFactory и записываем все обхекты в переменную dishes
-        DishFactory dishFactory = new DishFactory();
-        var dishes = dishFactory.CreateDish(lines);
-
-        //Создаем переменную по которой определяем от какого числа мы можем рекомендовать блюдо
-        var maxDate = DateTime.Now.AddDays(-userDays);
-        List<Dish> dateFilteredDishes = dishes.Where(d => d.LastPreparation < maxDate).ToList();
-
-        SortFactory sorter = new SortFactory();
-        var sorterType = sorter.CreateSorter(userType);
-
-        SortContext sortBy = new SortContext();
-        var resultDishes = sortBy.SortUsing(sorterType, dateFilteredDishes);
-
-        foreach (var item in resultDishes)
+        while (!foundDishes)
         {
-            Console.WriteLine(item.Type + item.Name + item.LastPreparation.ToString("dd/MM/yyyy"));
-        }
+            Console.Write("Введите число от скольки дней блюдо подходит к фильтрации: ");
+            if (int.TryParse(Console.ReadLine(), out int userDays))
+            {
+                //Создаем экземпляр класса DishFactory и записываем все обхекты в переменную dishes
+                DishFactory dishFactory = new DishFactory();
+                var dishes = dishFactory.CreateDish(lines);
 
-        RandomDishes myRandomDish = new RandomDishes();
-        string randomItem = myRandomDish.PickRandom(resultDishes);        
+                //Создаем переменную по которой определяем от какого числа мы можем рекомендовать блюдо
+                var maxDate = DateTime.Now.AddDays(-userDays);
+                List<Dish> dateFilteredDishes = dishes.Where(d => d.LastPreparation < maxDate).ToList();
 
-        Console.WriteLine($"Случайное блюдо:{randomItem}");
+                if (dateFilteredDishes.Any())
+                {
+                    Console.Write("Введите тип блюда (1/2): ");
+                    if (int.TryParse(Console.ReadLine(), out int userType))
+                    {
+                        foundDishes = true;
+
+                        SortFactory sorter = new SortFactory();
+                        var sorterType = sorter.CreateSorter((SortBy)userType);
+
+                        SortContext sortBy = new SortContext();
+                        var resultDishes = sortBy.SortUsing(sorterType, dateFilteredDishes);
+
+                        foreach (var item in resultDishes)
+                        {
+                            Console.WriteLine(item.Type + item.Name + item.LastPreparation.ToString("dd/MM/yyyy"));
+                        }
+
+                        RandomDishes myRandomDish = new RandomDishes();
+                        string randomItem = myRandomDish.PickRandom(resultDishes);        
+
+                        Console.WriteLine($"Случайное блюдо:{randomItem}");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Нет {userType} блюд. Попробуйте еще раз!");
+                    }                    
+                }
+                else
+                {
+                    Console.WriteLine($"Блюд, приготовленных {userDays} назад не найдено! Попробуйте снова!\n");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Ошибка ввода! Введите корректное число");
+            }
+        } 
     }   
 }
